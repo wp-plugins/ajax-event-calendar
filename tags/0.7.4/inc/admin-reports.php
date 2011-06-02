b7f041fb0f5c7aec3f82bbc09d9145ebf55d38ed
eranmiller <eranmiller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-02 16:31:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ajax-event-calendar@392344 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.7.4/inc/admin-reports.php
@@ -0,0 +1,13 @@
+<div class="wrap">
+<h2>Activity Report</h2>
+<?php
+$rows = $this->report_monthly_activity();
+if ( count( $rows ) ) {
+	foreach ( $rows as $row ) {
+		echo '<strong>' . $row->cnt . '</strong>: ' . $row->category . $this->plural($row->cnt) . '<br>';
+	}
+} else {
+	echo 'No events this month';
+}
+?>
+</div>
