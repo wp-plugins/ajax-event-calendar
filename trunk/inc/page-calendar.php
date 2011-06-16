23711d2169850e6b4ae2bb3948a6ea58882265dd
eranmiller <eranmiller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-16 06:16:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ajax-event-calendar@397807 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/inc/page-calendar.php
+++ trunk/inc/page-calendar.php
@@ -52,22 +52,81 @@ $options = get_option('aec_options');
 <script type="text/javascript" src="<?php echo AEC_PLUGIN_URL; ?>js/fullcalendar.min.js"></script>
 <script type="text/javascript" src="<?php echo AEC_PLUGIN_URL; ?>js/jquery.simplemodal.1.4.1.min.js"></script>
 <script type="text/javascript">
-	jQuery(document).ready(function() {		
-		<?php if (!$options['sidebar']) { ?>
-			jQuery('#content').css({ 'margin':'0' });
-		<?php }	?>
-		
+	jQuery(document).ready(function() {
+		<?php
+			if (!$options['sidebar']) {
+				echo "jQuery('#content').css({ 'margin':'0' });";
+			}
+		?>
 		var isFilter = (jQuery('#aec-filter li a').length > 0);
 		var calendar = jQuery('#aec-calendar').fullCalendar({
-			theme: true
-			, timeFormat: {
+			theme: true,
+			monthNames: ['<?php _e('January', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('February', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('March', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('April', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('May', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('June', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('July', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('August', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('September', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('October', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('November', AEC_PLUGIN_NAME); ?>',
+						 '<?php _e('December', AEC_PLUGIN_NAME); ?>'],
+			monthNamesShort: ['<?php _e('Jan', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Feb', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Mar', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Apr', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('May', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Jun', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Jul', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Aug', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Sep', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Oct', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Nov', AEC_PLUGIN_NAME); ?>',
+							  '<?php _e('Dec', AEC_PLUGIN_NAME); ?>'],
+			dayNames: ['<?php _e('Monday', AEC_PLUGIN_NAME); ?>',
+					   '<?php _e('Tuesday', AEC_PLUGIN_NAME); ?>',
+					   '<?php _e('Wednesday', AEC_PLUGIN_NAME); ?>',
+					   '<?php _e('Thursday', AEC_PLUGIN_NAME); ?>',
+					   '<?php _e('Friday', AEC_PLUGIN_NAME); ?>',
+					   '<?php _e('Saturday', AEC_PLUGIN_NAME); ?>',
+					   '<?php _e('Sunday', AEC_PLUGIN_NAME); ?>'],
+			dayNamesShort: ['<?php _e('Mon', AEC_PLUGIN_NAME); ?>',
+						    '<?php _e('Tue', AEC_PLUGIN_NAME); ?>',
+						    '<?php _e('Wed', AEC_PLUGIN_NAME); ?>',
+						    '<?php _e('Thu', AEC_PLUGIN_NAME); ?>',
+						    '<?php _e('Fri', AEC_PLUGIN_NAME); ?>',
+						    '<?php _e('Sat', AEC_PLUGIN_NAME); ?>',
+						    '<?php _e('Sun', AEC_PLUGIN_NAME); ?>'],
+			buttonText: {
+				today: '<?php _e('Today', AEC_PLUGIN_NAME); ?>',
+				month: '<?php _e('Month', AEC_PLUGIN_NAME); ?>',
+				week: '<?php _e('Week', AEC_PLUGIN_NAME); ?>',
+				day: '<?php _e('Day', AEC_PLUGIN_NAME); ?>'
+			},
+			allDayText: '<?php _e('All Day', AEC_PLUGIN_NAME); ?>',
+			/*
+			titleFormat: {
+				month: 'MMMM yyyy',
+				week: "d [ yyyy]{ '&#8212;'[ MMM] d MMM yyyy}",
+				day: 'dddd, d MMM, yyyy'
+			},
+			columnFormat: {
+				month: 'ddd',
+				week: 'ddd d/M',
+				day: 'dddd d/M'
+			},
+			axisFormat: 'H:mm',
+			*/
+			timeFormat: {
 				agenda: 'h:mmt{ - h:mmt}'
 				, '': 'h(:mm)t'
-			}
-			, firstHour: 8
-			, weekMode: 'liquid'
-			, editable: false
-			, eventRender: function(e, element) {
+			},
+			firstHour: 8,
+			weekMode: 'liquid',
+			editable: false,
+			eventRender: function(e, element) {
 				// check if filter is active
 				if (isFilter) {
 					var filter = jQuery('#aec-filter li.active').children().attr('class');
@@ -79,32 +138,31 @@ $options = get_option('aec_options');
 						}
 					}
 				}
-			}
-			, events: {
-				url: '<?php echo AEC_PLUGIN_URL; ?>inc/events.php'
-				, data: { 'edit' : 0 }
-				, type: 'POST'
+			},
+			events: {
+				url: '<?php echo AEC_PLUGIN_URL; ?>inc/events.php',
+				data: { 'edit' : 0 },
+				type: 'POST'
 				//, error: function(obj, type) { }
-			}
-			, header: {
-				left: 'prev,next today'
-				, center: 'title'
-				, right: 'month,agendaWeek'
-			}
-			, selectable: false
-			, selectHelper: false
-			, loading: function(b) {
+			},
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek'
+			},
+			selectable: false,
+			selectHelper: false,
+			loading: function(b) {
 				if (b) jQuery('#aec-loading').modal({ overlayId: 'aec-modal-overlay', close: false });
 				else jQuery.modal.close();
-			}
-			,eventClick: function(e) {
+			},
+			eventClick: function(e) {
 				eventDialog(e);
 			}
 		});
 		
 		if (isFilter) {
 			filter(jQuery('#aec-filter .all')); // filter: activate all
-			
 			jQuery('#aec-filter li a').click(function() {
 				filter(this);
 			});
@@ -118,14 +176,14 @@ $options = get_option('aec_options');
 		
 		function eventDialog(e) {		
 			jQuery('#aec-modal').modal({
-				overlayId: 'aec-modal-overlay'
-				, containerId: 'aec-modal-container'
-				, closeHTML: '<div class="close"><a href="#" class="simplemodal-close" title="<?php _e('Close Event Details', AEC_PLUGIN_NAME); ?>">x</a></div>'
-				, minHeight: 35
-				, opacity: 65
-				, position: ['0',]
-				, overlayClose: true
-				, onOpen: function (d) {
+				overlayId: 'aec-modal-overlay',
+				containerId: 'aec-modal-container',
+				closeHTML: '<div class="close"><a href="#" class="simplemodal-close" title="<?php _e('Close Event Details', AEC_PLUGIN_NAME); ?>">x</a></div>',
+				minHeight: 35,
+				opacity: 65,
+				position: ['0',],
+				overlayClose: true,
+				onOpen: function (d) {
 					var modal = this;
 					modal.container = d.container[0];
 					d.overlay.fadeIn(250, function () {
@@ -146,8 +204,8 @@ $options = get_option('aec_options');
 							}, 'json');
 						});
 					});
-				}
-				, onClose: function (d) {
+				},
+				onClose: function (d) {
 					var modal = this;
 					d.container.animate({ top:'-' + (d.container.height() + 20) }, 350, function () {
 						modal.close();
