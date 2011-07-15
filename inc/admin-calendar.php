5c11c3285eb4f1a5aaf14c2854179360f03d1b04
eranmiller <eranmiller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-15 21:10:37 +0000

    non-release
    git-svn-id: https://plugins.svn.wordpress.org/ajax-event-calendar/trunk@410456 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/admin-calendar.php
+++
@@ -1,9 +0,0 @@
-<div class="wrap">
-	<h2><?php _e('Calendar', AEC_PLUGIN_NAME); ?></h2>
-	<div id="aec-loading"><?php _e('Loading...', AEC_PLUGIN_NAME); ?></div>
-	<div id="aec-modal">
-		<div class="aec-title"></div>
-		<div class="aec-content"></div>
-	</div>
-	<div id="aec-calendar"</div>
-</div>
