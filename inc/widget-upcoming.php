16f05337fa01bb22920ca83137c08aeda49cb27d
eranmiller <eranmiller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-19 23:59:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ajax-event-calendar/trunk@412269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/widget-upcoming.php
+++ inc/widget-upcoming.php
@@ -65,7 +65,7 @@ class aec_upcoming_events extends WP_Widget{
 				$event->end_time 	= ajax_event_calendar::date_convert($event->end, AEC_DB_DATETIME_FORMAT, AEC_WP_TIME_FORMAT);
 				
 				// link to event			
-				$out .= '<li class="fc-event round5 cat"' . $event->category_id . '" onClick="$jq.eventDialog({\'id\':' . $event->id . '});">';
+				$out .= '<li class="fc-event round5 cat' . $event->category_id . '" onClick="$jq.eventDialog({\'id\':' . $event->id . '});">';
 				
 				if ($event->start_date != $event->end_date) {
 					// multiple day event, spanning all day
@@ -113,7 +113,7 @@ class aec_upcoming_events extends WP_Widget{
 	
 	/** @see WP_Widget::form */
 	function form($instance){
-		$instance = wp_parse_args( (array) $instance, array( 'title' => __('Three Week Agenda', AEC_PLUGIN_NAME), 'weeks' => '3', 'category' => 'all', 'format' => false) );
+		$instance = wp_parse_args((array) $instance, array( 'title' => __('Three Week Agenda', AEC_PLUGIN_NAME), 'weeks' => '3', 'category' => 'all'));
 		$title = $instance['title'];
 		$weeks = $instance['weeks'];
 		$category = $instance['category'];
